f0a415389b8a1dc28198bae9425d1cd6b9c1ff5b
JerryBin <1170373461@qq.com>
2024-09-17 10:04:52 +0800

    更新

--- docs/js/my.js
+++ docs/js/my.js
@@ -1,3 +1,7 @@
+/** 应用在外语>日语 >试题页面
+ * 点击按钮（id=“chakan_”+数字）
+ * 按钮的文字在“显示答案”和“隐藏答案”之间切换
+ * div（id=“daan_”+数字）同步显示答案或者隐藏答案 */
 function hidetimu(id){
     var btn = document.getElementById("chakan_"+id);
     var ansdiv = document.getElementById("daan_"+id);
