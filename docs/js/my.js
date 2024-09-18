c0d726468d720e1ec249384ae27962f1007966ef
JerryBin <1170373461@qq.com>
2024-09-18 19:42:26 +0800

    gengxin

--- docs/js/my.js
+++ docs/js/my.js
@@ -13,3 +13,11 @@ function hidetimu(id){
         btn.innerHTML = "查看答案";
     }
 }
+
+// 滚动到页面顶部的函数
+function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // 可选，平滑滚动效果
+    });
+  }
